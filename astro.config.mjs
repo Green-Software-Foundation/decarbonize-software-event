5495745d442dfbf426755026c1459079a1756b58
Osama Jandali <osamajandaly@gmail.com>
2022-10-23 00:02:27 +0300

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://decarbonize-software.github.io',
-    base: '/decarbonize-software',
+    site: 'https://decarb.greensoftware.foundation',
 });
